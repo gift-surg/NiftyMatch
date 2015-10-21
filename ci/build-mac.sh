596b83601a88f2069d22ea4d7ab763cb063b0b4e
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-21 12:02:59 +0100

    turned on building doc

--- ci/build-mac.sh
+++ ci/build-mac.sh
@@ -2,5 +2,5 @@
 rm -rf "$NiftyMatch_BUILD_DIR"
 mkdir -p "$NiftyMatch_BUILD_DIR"
 cd "$NiftyMatch_BUILD_DIR"
-cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" "$NiftyMatch_SOURCE_DIR"
+cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" -D BUILD_DOC=ON "$NiftyMatch_SOURCE_DIR"
 make -j
