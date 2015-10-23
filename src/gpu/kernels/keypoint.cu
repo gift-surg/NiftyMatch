bcc2ae48b7cfdd6623dca2ea73d47f7c657d21d7
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-23 14:24:15 +0100

    - renamed parameters for clarity - documented keypoint finding function

--- src/gpu/kernels/keypoint.cu
+++ src/gpu/kernels/keypoint.cu
@@ -224,28 +224,28 @@ __global__ void detect_keypoints(cudaTextureObject_t current, cudaTextureObject_
 }
 
 void find_keypoints(cudaTextureObject_t current, cudaTextureObject_t mask, cudaTextureObject_t down,
-                    cudaTextureObject_t up, const int width, const int height, const float peak_thresh,
+                    cudaTextureObject_t up, const int width, const int height, const float peak_threshold,
                     const float edge_threshold, const float xper, const float sigma_0,
                     const int num_dogs, const int dog, float4 *result, cudaStream_t stream)
 {
     dim3 blocks(16, 16);
     dim3 grid(DivUp(width, blocks.x), DivUp(height, blocks.y));
     detect_keypoints<<<grid, blocks, 0, stream>>>(current, mask, down, up, width, height,
-                                                  peak_thresh, edge_threshold, xper,
+                                                  peak_threshold, edge_threshold, xper,
                                                   sigma_0, num_dogs, dog, result);
     getLastCudaError("Keypoint detection launch failed");
 }
 
 
 void find_keypoints(cudaTextureObject_t current, cudaTextureObject_t down, cudaTextureObject_t up,
-                    const int width, const int height, const float peak_thresh,
+                    const int width, const int height, const float peak_threshold,
                     const float edge_threshold, const float xper, const float sigma_0,
                     const int num_dogs, int dog, float4 *result, cudaStream_t stream)
 {
     dim3 blocks(16, 16);
     dim3 grid(DivUp(width, blocks.x), DivUp(height, blocks.y));
     detect_keypoints<<<grid, blocks, 0, stream>>>(current, down, up, width, height,
-                                                  peak_thresh, edge_threshold, xper,
+                                                  peak_threshold, edge_threshold, xper,
                                                   sigma_0, num_dogs, dog, result);
     getLastCudaError("Keypoint detection launch failed");
 }
