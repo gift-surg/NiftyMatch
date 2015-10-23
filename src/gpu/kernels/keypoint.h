8e0c6d391599a87b1e139a1580d7e2a107d471a7
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-23 12:14:43 +0100

    readability edits

--- src/gpu/kernels/keypoint.h
+++ src/gpu/kernels/keypoint.h
@@ -4,15 +4,23 @@
 #include <cuda_runtime_api.h>
 
 // version without a mask
-void find_keypoints(cudaTextureObject_t current, cudaTextureObject_t down, cudaTextureObject_t up,
-                    const int width, const int height, const float peak_thresh, const float edge_threshold,
-                    const float xper, const float sigma_0, const int num_dogs, const int level,
-                    float4 *result, cudaStream_t stream=0);
+void find_keypoints(cudaTextureObject_t current,
+                    cudaTextureObject_t down, cudaTextureObject_t up,
+                    const int width, const int height,
+                    const float peak_thresh, const float edge_threshold,
+                    const float xper, const float sigma_0,
+                    const int num_dogs, const int level,
+                    float4 * result,
+                    cudaStream_t stream=0);
 
 // version with a mask
-void find_keypoints(cudaTextureObject_t current, cudaTextureObject_t mask, cudaTextureObject_t down,
-                    cudaTextureObject_t up, const int width, const int height, const float peak_thresh,
-                    const float edge_threshold, const float xper, const float sigma_0,
-                    const int num_dogs, const int level, float4 *result, cudaStream_t stream=0);
+void find_keypoints(cudaTextureObject_t current, cudaTextureObject_t mask,
+                    cudaTextureObject_t down, cudaTextureObject_t up,
+                    const int width, const int height,
+                    const float peak_thresh, const float edge_threshold,
+                    const float xper, const float sigma_0,
+                    const int num_dogs, const int level,
+                    float4 * result,
+                    cudaStream_t stream=0);
 
 #endif
