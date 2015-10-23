22b893db62e03a02e922d89f3008b8e7a101134e
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-23 14:53:31 +0100

    readability edits

--- src/gpu/kernels/bgra_2_gray.h
+++ src/gpu/kernels/bgra_2_gray.h
@@ -3,17 +3,26 @@
 #include <cuda.h>
 
 template<typename OutputType>
-void cuda_grayscale(const uchar4* bgra, OutputType * output, const int width, const int height,
-                    cudaStream_t stream=0);
+void cuda_grayscale(const uchar4 * bgra,
+                    OutputType * output,
+                    const int width, const int height,
+                    cudaStream_t stream = 0);
 
 template<typename OutputType>
-void cuda_extract_channel(const uchar4* bgra, OutputType * output, const int width, const int height,
-                          const int channel, cudaStream_t stream=0);
-
+void cuda_extract_channel(const uchar4 * bgra,
+                          OutputType * output,
+                          const int width, const int height,
+                          const int channel,
+                          cudaStream_t stream = 0);
 
 template<typename InputType>
-void cuda_put_channel(uchar4* bgra, const InputType * input, const int width, const int height,
-                      const int channel, cudaStream_t stream=0);
+void cuda_put_channel(uchar4 * bgra,
+                      const InputType * input,
+                      const int width, const int height,
+                      const int channel,
+                      cudaStream_t stream = 0);
 
-void cuda_set_alpha_to_const(uchar4* bgra, const int width, const int height,
-                             const unsigned char val=255, cudaStream_t stream=0);
+void cuda_set_alpha_to_const(uchar4 * bgra,
+                             const int width, const int height,
+                             const unsigned char val = 255,
+                             cudaStream_t stream = 0);
