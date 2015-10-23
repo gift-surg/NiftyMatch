43b08d4123cb45d2456f2362db1a8c0af488c733
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-23 14:33:00 +0100

    documented convolution

--- src/gpu/kernels/convolution.h
+++ src/gpu/kernels/convolution.h
@@ -4,6 +4,18 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+//!
+//! \brief Convolve \c image with \c kernel
+//! \param result convolved image saved here
+//! \param image
+//! \param buffer for optimal performance, e.g.
+//! via exploiting spatial locality
+//! \param width
+//! \param height
+//! \param kernel
+//! \param kernel_radius
+//! \param stream
+//!
 template<typename TYPE>
 void convolve(TYPE * result, const TYPE * image, TYPE * buffer,
               const int width, const int height,
