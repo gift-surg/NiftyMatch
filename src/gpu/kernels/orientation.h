d15bf9d749750195e541438e0ba073fa9225992b
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-23 14:33:48 +0100

    readability edits

--- src/gpu/kernels/orientation.h
+++ src/gpu/kernels/orientation.h
@@ -3,9 +3,11 @@
 
 #include "cuda_runtime_api.h"
 
-void detect_orientations(const float4* key_pts, const float2 *grad, const int num_pts,
+void detect_orientations(const float4 * key_pts, const float2 * grad,
+                         const int num_pts,
                          const int octave_width, const int octave_height,
                          float gauss_fac, const float xper,
-                         float2 *result, cudaStream_t stream=0);
+                         float2 * result,
+                         cudaStream_t stream = 0);
 
 #endif
