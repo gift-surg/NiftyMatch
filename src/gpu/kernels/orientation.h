7a5d9c45f384955e4e37a04916e8deea79f1aacc
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-23 14:45:18 +0100

    - renamed parameters for clarity - documented orientation

--- src/gpu/kernels/orientation.h
+++ src/gpu/kernels/orientation.h
@@ -3,10 +3,23 @@
 
 #include "cuda_runtime_api.h"
 
+//!
+//! \brief Detect keypoint orientations
+//! \param key_pts keypoints
+//! \param grad gradients
+//! \param num_pts number of keypoints
+//! \param octave_width
+//! \param octave_height
+//! \param gauss_factor
+//! \param xper level of scaling (i.e. as a result of
+//! downsampling)
+//! \param result orientations saved here
+//! \param stream
+//!
 void detect_orientations(const float4 * key_pts, const float2 * grad,
                          const int num_pts,
                          const int octave_width, const int octave_height,
-                         float gauss_fac, const float xper,
+                         float gauss_factor, const float xper,
                          float2 * result,
                          cudaStream_t stream = 0);
 
