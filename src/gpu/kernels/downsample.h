32ad830ad58c9f694dfd761ff4ff8155a37ef21b
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-23 11:06:47 +0100

    documented downsample

--- src/gpu/kernels/downsample.h
+++ src/gpu/kernels/downsample.h
@@ -4,6 +4,20 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+//!
+//! \brief Downsample an image by a factor of 2 for the scale-space
+//! representation.
+//! \param result
+//! \param result_width provided explicitly to avoid index-out-of-bounds
+//! type errors
+//! \param  provided explicitly to avoid index-out-of-bounds
+//! type errors
+//! \param source
+//! \param source_width
+//! \param source_height
+//! \param stream
+//! \sa PyramidData
+//!
 template<typename DataType>
 void downsample_by_2(DataType* result, const int result_width, const int result_height,
                      const DataType* source, const int source_width, const int source_height,
