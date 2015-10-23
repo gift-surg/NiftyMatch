ca0a3eacc690d40b65f10d7d9935be2dceb6e46e
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-23 14:54:41 +0100

    readability edits

--- src/gpu/kernels/transpose.h
+++ src/gpu/kernels/transpose.h
@@ -5,8 +5,10 @@
 #include <cuda_runtime.h>
 #include <cuda.h>
 
-template <typename TYPE>
-void transpose(TYPE *odata, const TYPE *idata, int width, int height,
-               cudaStream_t stream=0);
+template<typename TYPE>
+void transpose(TYPE * odata,
+               const TYPE * idata,
+               int width, int height,
+               cudaStream_t stream = 0);
 
 #endif
