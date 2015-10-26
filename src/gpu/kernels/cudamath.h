e784e806a2d6ad7dfaf67711bd455309df52be8d
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-10-26 10:46:47 +0000

    readability edits

--- src/gpu/kernels/cudamath.h
+++ src/gpu/kernels/cudamath.h
@@ -14,12 +14,16 @@ extern "C" int AlignUp(int a, int b);
 extern "C" int AlignDown(int a, int b);
 
 template<typename TYPE>
-void subtract(const TYPE * A, const TYPE * B, TYPE * C,
-              const int width, const int height, cudaStream_t stream=0);
+void subtract(const TYPE * A, const TYPE * B,
+              TYPE * C,
+              const int width, const int height,
+              cudaStream_t stream = 0);
 
 template<typename TYPE>
-void gradient(const TYPE * source, float2 * result,
-              const int width, const int height, cudaStream_t stream=0);
+void gradient(const TYPE * source,
+              float2 * result,
+              const int width, const int height,
+              cudaStream_t stream = 0);
 
 inline __host__ __device__ float mod_2pi_f (float x)
 {
