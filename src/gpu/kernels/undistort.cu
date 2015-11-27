7699ed3b9c9713a25e5d9a552ad80e8a53ff3636
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-11-27 11:06:17 +0000

    truly a kernel now

--- src/gpu/kernels/undistort.cu
+++ src/gpu/kernels/undistort.cu
@@ -8,8 +8,8 @@
 //! OpenCV undistort
 //! \param x original x coordinates
 //! \param y original y coordinates
-//! \param length number of coordinates (i.e.
-//! length of \c x and \c y)
+//! \param cols
+//! \param rows
 //! \param k1 see OpenCV undistort
 //! \param k2 see OpenCV undistort
 //! \param k3 see OpenCV undistort
@@ -22,28 +22,28 @@
 //! \param cy principal point (y) of camera
 //! matrix
 //! \param x_u distortion-corrected x
-//! coordinates
+//! coordinates (no boundary check performed!)
 //! \param y_u distortion-corrected y
-//! coordinates
+//! coordinates (no boundary check performed!)
 //!
 __global__ void undistort(const float * x, const float * y,
-                          size_t length,
+                          const size_t cols, const size_t rows,
                           const float k1, const float k2, const float k3,
                           const float fx, const float fy,
                           const float cx, const float cy,
                           float * x_u, float * y_u)
 {
-    float r = 0, tmp = 0;
-    for (size_t i = 0; i < length; i++) {
-        r = powf(x[i], 2) + powf(y[i], 2);
-        // common coefficient for multiplication
-        tmp = 1 + k1 * powf(r, 2) + k2 * powf(r, 4) + k3 * powf(r, 6);
+    const size_t i = __mul24(blockDim.x, blockDim.x) + threadIdx.x;
+    const size_t j = __mul24(blockDim.y, blockDim.y) + threadIdx.y;
+    const size_t pos = j * cols + i; // cols = width, rows = height
 
-        x_u[i] = (x[i] - cx) / fx;
-        x_u[i] *= tmp;
-        y_u[i] = (y[i] - cy) / fy;
-        y_u[i] *= tmp;
-    }
+    float r = powf(x[pos], 2) + powf(y[pos], 2);
+    // common coefficient for multiplication
+    float tmp = 1 + k1 * powf(r, 2) + k2 * powf(r, 4) + k3 * powf(r, 6);
+    x_u[pos] = (x[pos] - cx) / fx;
+    x_u[pos] *= tmp;
+    y_u[pos] = (y[pos] - cy) / fy;
+    y_u[pos] *= tmp;
 }
 
 void cuda_undistort(const float * x, const float * y,
@@ -65,7 +65,7 @@ void cuda_undistort(const float * x, const float * y,
           cx = camera_matrix[2],
           cy = camera_matrix[3];
     undistort<<<grid, blocks, 0, stream>>>(x, y,
-                                           rows * cols,
+                                           cols, rows,
                                            k1, k2, k3,
                                            fx, fy,
                                            cx, cy,
