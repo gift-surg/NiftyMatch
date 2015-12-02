2752b36ee051250f9cebb1f076d88e6f45595afb
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-12-02 11:08:46 +0000

    - removed misleading (and now irrelevant) TODO - removed duplicate doxygen - revised doxygen for function declaration

--- src/gpu/kernels/undistort.cu
+++ src/gpu/kernels/undistort.cu
@@ -3,47 +3,6 @@
 #include <vector_types.h>
 #include <helper_cuda.h>
 
-//!
-//! \brief Apply distortion correction as in
-//! OpenCV undistort
-//! \param x original x coordinates
-//! \param y original y coordinates
-//! \param cols
-//! \param rows
-//! \param distortion coeffs k1, k2, k3 as in
-//! OpenCV undistort
-//! \param camera_matrix fx, fy, cx, cy as in
-//! OpenCV undistort
-//! \param u distortion-corrected x
-//! coordinates (no boundary check performed!)
-//! \param v distortion-corrected y
-//! coordinates (no boundary check performed!)
-//!
-
-//!
-//! \brief Compute distortion correction map
-//! from undistorted image coordinates \c u and
-//! \c v to original image coordinates \c x and
-//! \c y, using same process as OpenCV undistort)
-//! \param x original x coordinates (used as
-//! initial values for \c u)
-//! \param y original y coordinates (used as
-//! initial values for \c v)
-//! \param cols not used for border checks, so
-//! resulting \c u values might be out of
-//! bounds!
-//! \param rows not used for border checks, so
-//! resulting \c v values might be out of
-//! bounds!
-//! \param camera_matrix fx, fy, cx, and cy as
-//! in OpenCV undistort
-//! \param distortion_coeffs k1, k2, and k3 as
-//! in OpenCV undistort
-//! \param u for each new position \c u_i, use
-//! \c u[u_i] from original image
-//! \param v for each new potision \c v_i, use
-//! \c v[v_i] from original image
-//!
 __global__ void undistort(const float * x, const float * y,
                           const size_t cols, const size_t rows,
                           const float * distortion_coeffs,
@@ -102,5 +61,4 @@ void cuda_undistort(const float * x, const float * y,
                                            camera_matrix,
                                            u, v);
     getLastCudaError("Undistort kernel launch failed");
-    // TODO add resample!
 }
--- src/gpu/kernels/undistort.h
+++ src/gpu/kernels/undistort.h
@@ -7,27 +7,30 @@
 //! \brief Compute distortion correction map
 //! from undistorted image coordinates \c u and
 //! \c v to original image coordinates \c x and
-//! \c y, using same process as OpenCV undistort)
-//! \param x original x coordinates
-//! \param y original y coordinates
+//! \c y, using same process as OpenCV undistort
+//! \param x original x coordinates (used as
+//! initial values for \c u)
+//! \param y original y coordinates (used as
+//! initial values for \c v)
 //! \param cols not used for border checks, so
 //! resulting \c u values might be out of
 //! bounds!
 //! \param rows not used for border checks, so
 //! resulting \c v values might be out of
 //! bounds!
-//! \param camera_matrix fx, fy, cx, and cy as
-//! in OpenCV undistort
-//! \param distortion_coeffs k1, k2, and k3 as
-//! in OpenCV undistort
+//! \param camera_matrix \c fx, \c fy, \c cx,
+//! and \c cy as in OpenCV undistort
+//! \param distortion_coeffs \c k1, \c k2, and
+//! \c k3 as in OpenCV undistort
 //! \param u for each new position \c u_i, use
 //! \c u[u_i] from original image
-//! \param v for each new potision \c v_i, use
+//! \param v for each new position \c v_i, use
 //! \c v[v_i] from original image
+//! \param stream
 //!
 void cuda_undistort(const float * x, const float * y,
                     const size_t cols, const size_t rows,
                     const float * camera_matrix,
                     const float * distortion_coeffs,
-                    float * x_u, float * y_u,
+                    float * u, float * u,
                     cudaStream_t stream = 0);
