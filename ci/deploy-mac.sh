650e8ce2ce8ca244ab7e8e697124af3e4239a1f0
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-12-10 11:43:49 +0000

    using 5-rename-project branch of renamed project instead of dev in CI scripts, just to make sure everything is OK with that

--- ci/deploy-mac.sh
+++ ci/deploy-mac.sh
@@ -2,7 +2,7 @@
 cd "$NiftyMatch_BUILD_DIR"
 make install
 rm -rf "$Test_REPO_DIR"
-git clone git@cmiclab.cs.ucl.ac.uk:GIFT-Surg/NiftyMatch-Test.git "$Test_REPO_DIR" --branch dev
+git clone git@cmiclab.cs.ucl.ac.uk:GIFT-Surg/NiftyMatch-Test.git "$Test_REPO_DIR" --branch 5-rename-project
 rm -rf "$Test_BUILD_DIR"
 mkdir -p "$Test_BUILD_DIR"
 cd "$Test_BUILD_DIR"
